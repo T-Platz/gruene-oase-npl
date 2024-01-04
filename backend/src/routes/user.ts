d3661dbacef4e3fdf7fd76c1503cb2a9e7053c7f
Timon Platz <>
2024-01-04 18:21:18 +0100

    Refine endpoints

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,29 +1,36 @@
 import express, { Router, Request, Response } from 'express';
-import { User } from '../mongodb';
-import { JwtPayload } from './auth';
+import { User } from '../db/mongodb';
 
 export const userRouter: Router = express.Router();
 
 userRouter.get('/', async (req: Request, res: Response) => {
     console.log('/user GET');
-    
-    const auth = req.auth as JwtPayload;
-    const user = await User.findById(auth.userId);
-    if (!user)
-        return res.status(404).send('');
 
-    return res.send(user);
+    // Check auth
+    if (!req.auth)
+        return res.status(403).send();
+
+    // Check if user exists
+    const user = await User.findById(req.auth.userId).populate('lots');
+    if (!user)
+        return res.status(404).send();
+
+    return res.send({ user: user });
 });
 
 userRouter.patch('/', async (req: Request, res: Response) => {
     console.log('/user PATCH');
-    
-    const auth = req.auth as JwtPayload;
-    const user = await User.findById(auth.userId);
+
+    // Check auth
+    if (!req.auth)
+        return res.status(403).send();
+
+    // Check if user exists
+    const user = await User.findById(req.auth.userId).populate('lots');
     if (!user)
-        return res.status(404).send('');
+        return res.status(404).send();
 
     // Todo: Update user here
 
-    return res.send(user);
+    return res.send({ user: user });
 });
