3ee1ebcfcb06a2e996783985197034c5ca88d11b
Timon Platz <>
2024-01-07 18:19:29 +0100

    Fix wording for single notification

--- frontend/src/components/other/AllNotifications.tsx
+++ frontend/src/components/other/AllNotifications.tsx
@@ -9,9 +9,15 @@ function AllNotifications(props: AllNotificationsProps) {
     return(
         <div className="flex flex-row items-center">
             {props.number === 0 ? <Check sx={{color: '#97d045'}}/> : <ReportProblemSharp sx={{color: '#e55523'}}/>}
-            <Typography variant="h6" sx={{paddingLeft: '8px', color: props.number === 0 ? '#97d045' :  '#e55523'}}>{props.number === 0 ? 'Mit deinen Gärten ist alles in Ordnung!' : `${props.number} neue Meldungen!`}</Typography>
+            <Typography variant="h6" sx={{paddingLeft: '8px', color: props.number === 0 ? '#97d045' :  '#e55523'}}>
+                {
+                    props.number <= 0 ? 'Mit deinen Gärten ist alles in Ordnung!' :
+                    props.number === 1 ? '1 neue Meldung!' :
+                    `${props.number} neue Meldungen`
+                }
+            </Typography>
         </div>
-    )
+    );
 }
 
 export default AllNotifications;
